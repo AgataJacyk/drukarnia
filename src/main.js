e6980ab1e50c078290f2dc58cb3443c0d5c5b1f8
Dominika <pimmortals@gmail.com>
2023-05-05 18:37:37 +0200

    nowe komponenty i wywalone stare

--- src/main.js
+++ src/main.js
@@ -1,11 +1,5 @@
-import './assets/main.css'
+const app = createApp(App);
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+app.use(router);
 
-const app = createApp(App)
-
-app.use(router)
-
-app.mount('#app')
+app.mount("#app");
