ccd47d4931ff7573a81c3d0b72e6baea3a292e8a
Dominika <pimmortals@gmail.com>
2023-05-05 19:44:00 +0200

    subcomponents which does not work

---
+++ src/components/ONas.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>O Nas</h1>
+    <o-firmie />
+    <artysci />
+  </div>
+</template>
+
+<script>
+import OFirmie from "./components/OFirmie.vue";
+import Artysci from "./components/Artysci.vue";
+
+export default {
+  name: "ONas",
+  components: {
+    OFirmie,
+    Artysci,
+  },
+};
+</script>
